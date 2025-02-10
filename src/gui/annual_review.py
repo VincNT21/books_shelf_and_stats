261b194641930b0019252d0931c51af7917986cc
VincNT21 <vincnt21@pm.me>
2025-02-10 15:28:57 +0100

    begin working on annual review display : organization of window and drawing the grid

--- src/gui/annual_review.py
+++ src/gui/annual_review.py
@@ -1,4 +1,28 @@
-from gui.graphics import Grid
+import tkinter as tk
+from tkinter import ttk
+from gui.graphics import Grid, Window
 
-class BooksByMonth:
+
+def create_annual_review(year):
+    window = Window(600, 800, "Annual review")
+    root = window.get_root()
+
+
+    title_label = ttk.Label(root, text= f"Reading stats of {year}", font= ("Arial", 20))
+    title_label.grid(row = 0, column= 0, columnspan= 2)
+
+    books_canvas = tk.Canvas(root, width=450, height=300, bg="white")
+    books_canvas.grid(row= 1, column= 0)
+    grid_title = books_canvas.create_text((150, 40), text= "Books By Month", font=("Arial", 16))
+    empty_grid = Grid(20, 50, 6, 10, 40, 40, books_canvas)
+    legend_canvas = tk.Canvas(root, width=150, height=300, bg="white")
+    legend_canvas.grid(row=1, column=1)
+    
+    pages_canvas = tk.Canvas(root, width=600, height=400, bg="white")
+    pages_canvas.grid(row= 2, column=0, columnspan= 2)
+
+    window.run()
+
+if __name__ == "__main__":
+    create_annual_review()
     
--- src/gui/graphics.py
+++ src/gui/graphics.py
@@ -1,29 +1,29 @@
 from tkinter import Tk, Canvas
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width, height, title):
         self.__root = Tk()
-        self.__root.title("Annual review")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__root.title(title)
+        self.__root.geometry(f"{width}x{height}")
+
+    def get_root(self):
+        return self.__root  
+
+    def run(self):
+        self.__root.mainloop()
+
+
+class WhiteCanvas:
+    def __init__(self, window, width, height):
+        self.__canvas = Canvas(window, width= width, height= height, bg="white")
         self.__canvas.pack()
-        self.__running = False
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running is True:
-            self.redraw()
-        print("Window closed...")
-
-    def close(self):
-        self.__running = False
 
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
+
+    def redraw(self):
+        self.__canvas.update_idletasks()
+        self.__canvas.update()
 
 
 class Point:
@@ -37,19 +37,19 @@ class Line:
         self.p1 = p1
         self.p2 = p2
 
-    def draw(self, Canvas, fill_color="black"):
-        Canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
 
 class Cell:
-    def __init__(self, window=None):
+    def __init__(self, canvas=None):
         self._x1 = None
         self._x2 = None
         self._y1 = None
         self._y2 = None
-        self._win = window
+        self._canvas = canvas
 
     def draw(self, x1, x2, y1, y2):
-        if self._win is None: # in test case
+        if self._canvas is None: # in test case
             return
         self._x1 = x1
         self._x2 = x2
@@ -59,13 +59,13 @@ class Cell:
         right_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
         top_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
         bottom_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-        self._win.draw_line(left_wall)
-        self._win.draw_line(right_wall)
-        self._win.draw_line(top_wall)
-        self._win.draw_line(bottom_wall)
+        left_wall.draw(self._canvas)
+        right_wall.draw(self._canvas)
+        top_wall.draw(self._canvas)
+        bottom_wall.draw(self._canvas)
 
 class Grid:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, window=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, canvas=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -73,14 +73,14 @@ class Grid:
         self._num_cols = num_cols
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
-        self._win = window
+        self._canvas = canvas
         self._create_cells()
 
     def _create_cells(self):
         for i in range(self._num_cols):
             cells_col = []
             for j in range(self._num_rows):
-                cells_col.append(Cell(self._win))
+                cells_col.append(Cell(self._canvas))
             self._cells.append(cells_col)
         
         for i in range(self._num_cols):
@@ -93,5 +93,4 @@ class Grid:
             y1 = self._y1 + (self._cell_size_y * j)
             y2 = self._y1 + (self._cell_size_y * (j + 1))
             self._cells[i][j].draw(x1, x2, y1, y2)
-            self._win.redraw()
 
